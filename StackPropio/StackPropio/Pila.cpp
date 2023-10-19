5e8a45316d53f4b64043f01c0e55fd910f751c45
fransandope <Fransan12p@gmail.com>
2023-10-18 20:05:17 -0600

    Comprobacion C y X

---
+++ StackPropio/StackPropio/Pila.cpp
@@ -0,0 +1,34 @@
+#include "Pila.h"
+//extraer cola
+void Pila::Push(String^ value) {
+    Node^ newNode = gcnew Node(value);
+    if (isEmpty()) {
+        start = newNode;
+    }
+    else {
+        newNode->next = start;
+        start = newNode;
+    }
+    count++;
+}
+
+bool Pila::isEmpty() {
+    return count == 0;
+}
+
+String^Pila::Pop() {
+    if (!isEmpty()) {
+        Node^ temp = start;
+        start = start->next;
+        count--;
+        return temp->dato;
+    }
+    return nullptr;
+}
+
+String^ Pila::Peek() {
+    if (!isEmpty()) {
+        return start->dato;
+    }
+    return nullptr;
+}
